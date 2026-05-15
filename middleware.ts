5c7f735b6225ee760f093d0c74bd118b4f97f078
doxaodoi <doxaodoi222@email.com>
2026-05-15 22:28:06 +0000

    #change

--- middleware.ts
+++ middleware.ts
@@ -28,5 +28,5 @@ export default auth(function middleware(req: NextRequest & { auth: { user?: { ro
 });
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon\\.ico|.*\\.(?:png|jpg|jpeg|svg|gif|webp|ico)$).*)"],
 };
